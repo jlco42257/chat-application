d116afec05e8e649d8a64d28167274151df6279f
jl-tab <jlco42257@gmail.com>
2024-10-21 00:11:47 -0500

    view: register, login;

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import Login from './views/Login.jsx'
+import Register from './views/Register.jsx'
 
 function App() {
   return (
-    <main className='bg-slate-950 min-h-svh'>
-      <Login/>
+    <main className='main'>
+      <Register/>
     </main>
   )
 }
--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,15 +1,16 @@
 import './Login.css'
-function Login() {
+function Register() {
   return (
     <section className="login__container">
-      <h1 className="login__title">Login</h1>
+      <h1 className="login__title">Register</h1>
       <form className="login__form">
+        <input className="form__input" id="name" name="name" placeholder="name" type="text"/>
         <input className="form__input" id="email" name="email" placeholder="email" type="email"/>
         <input className="form__input" id="password" name="password" placeholder="password" type="password"/>
-        <button type="submit" className="form__submit">Login</button>
+        <button type="submit" className="form__submit">Register</button>
       </form>
     </section>
   )
 }
 
-export default Login
+export default Register
